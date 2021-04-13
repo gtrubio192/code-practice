e2b4caa7471620b16df0fc4ee24a94432a9c6bd1
Gabe Rubio <gtrubio192@gmail.com>
2021-04-13 14:45:01 -0600

    adding iterator problem

--- rangeSumBST.js
+++ rangeSumBST.js
@@ -12,6 +12,9 @@
  * @param {number} high
  * @return {number}
  */
+
+// Given the root node of a binary search tree, 
+// return the sum of values of all nodes with a value in the range [low, high]
 var rangeSumBST = function(curr, low, high) {
     // in order traversal - left root right
     if(curr && curr.val >= low && curr.val <= high) {
